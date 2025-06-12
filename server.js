102e33a5467407ba42443a4b8b5256e352b742da
Reginaldo Machado <111655927+ReginadoMachado@users.noreply.github.com>
2025-06-12 19:58:47 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-const swaggerUi = require('swagger-ui-express');
-//carrega a documentação swagger
-const YAMl = require('yamljs');
-//carrega a documentação do swagger
-const swaggerDocument = YAMl.load('./swagger/swagger.yml')
-
-//adiciona o swagger UI
-app.use('./swagger/swagger.yml', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-
 // Permite que o Express entenda JSON no corpo da requisição
 app.use(express.json());
 
